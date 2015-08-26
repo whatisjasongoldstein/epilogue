8c1d57174f9c8944cb7f8c60a569410e528ff8ba
Jason Goldstein <jason@betheshoe.com>
2015-08-25 22:53:29 -0400

    No more open secrets

--- epilogue/app.py
+++ epilogue/app.py
@@ -10,8 +10,8 @@ DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, '../db/epilogue.sqlite')
 DEBUG = False
 
 # TODO: change and hide these
-SECRET_KEY = "Don't tell anyone!"  # Used by Flask to encrypt session cookie.
-secret_webhook_uuid = "44ff110a-b3de-4722-814e-4da15f65b2ae"
+SECRET_KEY = os.environ['EPILOGUE_SECRET_KEY']  # Used by Flask to encrypt session cookie.
+secret_webhook_uuid = os.environ['EPILOGUE_WEBHOOK_UUID']
 
 app = Flask(__name__)
 app.config.from_object(__name__)
